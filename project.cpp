ffcbada5b6617eee225debaae48cbb10a09bfcf5
Anvit <anvitchauhan182007@gmail.com>
2025-12-14 14:53:41 +0530

    Add menu-driven structure for simulator

--- project.cpp
+++ project.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
-    cout << "Virtual Memory Management Simulator\n";
+    cout << "\n=== Virtual Memory Management Simulator ===\n";
+
+    while (true) {
+        cout << "\n1. Segmentation\n";
+        cout << "2. Paging\n";
+        cout << "3. Memory Fragmentation\n";
+        cout << "4. Exit\n";
+        cout << "Choose option: ";
+
+        int choice;
+        cin >> choice;
+
+        if (choice == 4) {
+            cout << "Exiting simulator...\n";
+            break;
+        } else {
+            cout << "Feature under development.\n";
+        }
+    }
+
     return 0;
 }
